c6e4c1f22f09f0e670249c0c402bf1dd5748d32d
yangmengmeng1997 <1459834870@qq.com>
2022-05-05 15:27:15 +0800

    完成任务到第七章

--- src/test/java/com/newcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/newcoder/community/CommunityApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringBootVersion;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -67,6 +68,11 @@ class CommunityApplicationTests implements ApplicationContextAware {
 		System.out.println(alphaDao);
 		System.out.println(alphaService);
 		System.out.println(simpleDateFormat);
+	}
+
+	@Test
+	public void test(){
+		System.out.println(SpringBootVersion.getVersion());
 	}
 
 	//******************************************************************************************************
